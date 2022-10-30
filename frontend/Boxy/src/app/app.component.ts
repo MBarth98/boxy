ca9a194834ba20c2fbc95bb56ca0eec8aba6ac82
Mads Mandahl-Barth <mads-barth@live.dk>
2022-10-30 17:02:22 +0100

    cleaned the template. deleted stuff I am not interested in.

--- frontend/Boxy/src/app/app.component.ts
+++ frontend/Boxy/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+// input output event-emitter
+// @Input() name : string;
+// @Output() event = new EventEmitter();
+// html <app-child (event)="onEvent()">{{title}}</app-child>
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
