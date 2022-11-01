19f3c21867724f0952dbbc5f8c5644b02cffe03d
Mads Mandahl-Barth <mads-barth@live.dk>
2022-11-01 09:24:06 +0100

    seperated into backend and frontend

--- frontend/Boxy/src/app/app.module.ts
+++ frontend/Boxy/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser';
+import {} from '@angular/forms'
+import {} from '@angular/material'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxCardComponent } from './components/box-card/box-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
